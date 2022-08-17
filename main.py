75e2532a1f57402d607cb1c5bd4d372be999e199
Fredbear1 <elkinposada03@gmail.com>
2022-08-17 17:34:15 -0500

    se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,14 @@
+#HOLA ME LLAMO COMENTARIO DE LINEA
+
+'''
+HOLA SOY UN COMENTARIO DE BLOQUE
+'''
+#DATOS PRIMITIVOS (BASICOS)
+nombre='Maga'
+edad=39
+estatura=1.65
+hinchadelmejor=True
+
+#SALIDAS
+print("hola: ",nombre,"tu estatura",estatura)
+print(f'hola {nombre} tu estatura es {estatura}')
